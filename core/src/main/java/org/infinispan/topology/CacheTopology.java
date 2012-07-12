be88fecfbab0d27118e83cad8cc77214f93dcf3a
Dan Berindei <dan@infinispan.org>
2012-07-12 15:38:25 +0300

    New interfaces for the global components that take execute the rebalance op.

---
+++ core/src/main/java/org/infinispan/topology/CacheTopology.java
@@ -0,0 +1,14 @@
+package org.infinispan.topology;
+
+import org.infinispan.distribution.newch.AdvancedConsistentHash;
+import org.infinispan.distribution.newch.ConsistentHash;
+
+/**
+ * The status of a cache from a distribution/state transfer point of view.
+ * The pending CH can be {@code null} if we don't have a state transfer in progress.
+ */
+class CacheTopology {
+   int version;
+   ConsistentHash currentCH;
+   ConsistentHash pendingCH;
+}
