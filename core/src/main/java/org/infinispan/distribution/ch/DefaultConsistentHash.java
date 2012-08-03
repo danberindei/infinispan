01a54ba755340b862e2b5bd038915969be2cf071
Adrian Nistor <anistor@redhat.com>
2012-08-03 15:16:28 +0300

    Reused Ids.DEFAULT_CONSISTENT_HASH

--- core/src/main/java/org/infinispan/distribution/ch/DefaultConsistentHash.java
+++ core/src/main/java/org/infinispan/distribution/ch/DefaultConsistentHash.java
@@ -227,7 +227,7 @@ public class DefaultConsistentHash implements ConsistentHash {
 
       @Override
       public Integer getId() {
-         return Ids.NEW_DEFAULT_CONSISTENT_HASH;
+         return Ids.DEFAULT_CONSISTENT_HASH;
       }
 
       @Override
