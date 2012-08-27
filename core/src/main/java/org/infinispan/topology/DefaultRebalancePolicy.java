f6874aafef6f9f3f0f0f61e706481af901657c74
Dan Berindei <dan@infinispan.org>
2012-08-27 19:27:13 +0300

    Don't forget to trigger a rebalance after a leave

--- core/src/main/java/org/infinispan/topology/DefaultRebalancePolicy.java
+++ core/src/main/java/org/infinispan/topology/DefaultRebalancePolicy.java
@@ -157,9 +157,10 @@ public class DefaultRebalancePolicy implements RebalancePolicy {
                updateConsistentHash(cacheName, cacheStatus, cacheTopology);
             }
 
-            if (!cacheStatus.getJoiners().isEmpty()) {
-               // In rare cases we get the join request from a new node before JGroups has installed the new view
-               // If that happens, we ignore the joiner until the view containing it has been installed.
+            if (!isBalanced(cacheStatus.getCacheTopology().getCurrentCH()) || !cacheStatus.getJoiners().isEmpty()) {
+               // Rebalance after a leave.
+               // Also, in rare cases we get the join request from a new node before JGroups has installed the new view
+               // If that happens, we re-trigger the rebalance after the view containing it has been installed.
                triggerRebalance(cacheName, cacheStatus);
             }
          }
@@ -265,7 +266,7 @@ public class DefaultRebalancePolicy implements RebalancePolicy {
          ConsistentHash updatedMembersCH = chFactory.updateMembers(currentCH, newMembers);
          ConsistentHash balancedCH = chFactory.rebalance(updatedMembersCH);
          if (balancedCH.equals(currentCH)) {
-            log.tracef("The balanced CH is the same as the current CH, stopping rebalance");
+            log.tracef("The balanced CH is the same as the current CH, not rebalancing");
             return;
          }
          CacheTopology cacheTopology = new CacheTopology(newTopologyId, currentCH, balancedCH);
